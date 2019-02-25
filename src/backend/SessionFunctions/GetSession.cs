d879e89826262e9f1faaaad48cf8bbe3ffac6b61
Boris Wilhelms <mail@wille-zone.de>
2019-02-25 10:44:32 +0100

    Adds function to retrieve list of sessions

---
+++ src/backend/SessionFunctions/GetSession.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SessionFunctions
+{
+    public static class GetSessionFunctions
+    {
+        [FunctionName("GetSession")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "sessions")] HttpRequest req,
+            [CosmosDB(databaseName: "MBAWorkshop2019", collectionName: "Sessions",
+                ConnectionStringSetting = "CosmosDbConnection")] IEnumerable<Session> sessions)
+        {
+            return new OkObjectResult(sessions.Select(s => new { s.Id, s.Title }));
+        }
+    }
+}
---
+++ src/backend/SessionFunctions/Session.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace SessionFunctions
+{
+
+    public class Session
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Abstract { get; set; }
+
+        public string Speaker { get; set; }
+    }
+}
